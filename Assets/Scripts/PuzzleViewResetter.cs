b3fe48c92acf8b9d9154e1d36177200bb2d15fe8
KnightWhoSaysNi <nnikola.milovanovic@gmail.com>
2017-10-29 19:05:41 +0100

    PuzzleViewResetter added

---
+++ Assets/Scripts/PuzzleViewResetter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleViewResetter : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToActivate;
+    [SerializeField] private GameObject[] objectsToDeactivate;
+    [SerializeField] private AudioClip resetAudio;
+
+    private void OnDisable()
+    {
+        objectToActivate.SetActive(true);
+        AudioManager.Instance.PlayAudioClip(resetAudio);
+
+        for (int i = 0; i < objectsToDeactivate.Length; i++)
+        {
+            objectsToDeactivate[i].SetActive(false);
+        }
+    }
+}
