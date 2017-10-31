f6bcaa5ce973bb0c9b5990aa0a4006aa16c9b514
KnightWhoSaysNi <nnikola.milovanovic@gmail.com>
2017-10-31 18:27:56 +0100

    Boat.cs added

---
+++ Assets/Scripts/Window and Boat/Boat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boat : MonoBehaviour
+{
+    [SerializeField] private GameObject inventory;
+
+    private void OnEnable()
+    {
+        inventory.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        inventory.SetActive(true);
+    }
+}
