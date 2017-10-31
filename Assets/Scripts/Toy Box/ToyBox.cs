32ba731dab50fd6238108639a204dbd9dbd09311
KnightWhoSaysNi <nnikola.milovanovic@gmail.com>
2017-10-31 18:27:35 +0100

    ToyBox.cs added

---
+++ Assets/Scripts/Toy Box/ToyBox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToyBox : MonoBehaviour
+{
+    [SerializeField] private GameObject toyBoxUnlocked;
+
+    private void OnMouseDown()
+    {
+        if (InventoryManager.Instance.IsItemSelected(InventoryItemType.PadlockKey))
+        {
+            toyBoxUnlocked.SetActive(true);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
