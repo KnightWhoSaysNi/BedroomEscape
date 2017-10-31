8f59e5fbf9c8dcfa5cfa99498a1a3830dcb37040
KnightWhoSaysNi <nnikola.milovanovic@gmail.com>
2017-10-31 01:45:50 +0100

    InventoryItem.cs and ItemCombination.cs added

---
+++ Assets/Scripts/Puzzle and Inventory Items/ItemCombination.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct ItemCombination
+{
+    public string title;
+    [Space(5)]
+    public InventoryItemType firstItem;
+    public InventoryItemType secondItem;
+    [Space(10)]
+    public InventoryItem gainedItem;
+}
---
+++ Assets/Scripts/Puzzle and Inventory Items/PuzzleItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class PuzzleItem : MonoBehaviour
+{
+    [SerializeField] private InventoryItem inventoryItem;
+
+    private void OnMouseDown()
+    {
+        AudioManager.Instance.PlayPuzzleSolvedAudio();
+        InventoryManager.Instance.RegisterItemAcquisition(inventoryItem);
+        Destroy(this.gameObject, 0.1f);
+    }
+}
